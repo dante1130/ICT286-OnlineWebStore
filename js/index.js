5fe8198342a4f4afb686a0f92d9b25904f0d5093
Andrew <andrewdante2@gmail.com>
2021-10-16 18:01:21 +0800

    Added burger nav for mobile

--- js/index.js
+++ js/index.js
@@ -2,13 +2,17 @@ $('document').ready(function() {
     switchPage("home.html");
 })
 
-$('.home-link').on('click', (e) => {
+$('.home-link').on('click', () => {
     switchPage("home.html");
 });
 
 $('.nav-link').on('click', (e) => {
     switchPage("search.html");
     search($(e.target).text());
+});
+
+$('.burger-nav').on('click', () => {
+    $('.nav-links').toggleClass('nav-active');
 });
 
 function switchPage(pageRef) {
