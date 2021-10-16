4fa0043116b2c3d1b2984f4d5a5c6f76199ee334
Andrew <andrewdante2@gmail.com>
2021-10-16 15:46:02 +0800

    Added slider to homepage

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-function switchPage(pageRef) {
-    $.get(pageRef, (data) => {
-        $('main').html(data);
-    });
-}
+$('document').ready(function() {
+    switchPage("home.html");
+})
+
+$('.home-link').on('click', (e) => {
+    switchPage("home.html");
+});
 
 $('.nav-link').on('click', (e) => {
     switchPage("search.html");
     search($(e.target).text());
 });
+
+function switchPage(pageRef) {
+    $.get(pageRef, (data) => {
+        $('main').html(data);
+    });
+}
