6d823ad7b2fe0daf3def9106c3b8f943d368e2b4
33670851 <33670851@ceto.murdoch.edu.au>
2021-10-28 01:32:53 +0800

    Finished search product

--- js/search.js
+++ js/search.js
@@ -1,10 +1,23 @@
 function search(input) {
     $.get("php/search.php", { userinput: input }, (data) => {
-        console.log(data);
+        displayItems(JSON.parse(data));
     });
 }
 
-$('#search-form').on('submit', (e) => {
-    e.preventDefault();
+function displayItems(products) {
+    $('.grid-container').empty();
+
+    products.forEach((element) => {
+        let productDiv = $('<div/>')
+                            .addClass("grid-item")
+                            .html(`<h2>${element.ProductName}</h2>
+                                   <p>${element.ProductDesc}</p>
+                                   <p>$${element.Price}</p>`);
+
+        $('.grid-container').append(productDiv);
+    });   
+}
+
+$('#search').on('input', (e) => {
     search($('#search').val());
 });
--- php/search.php
+++ php/search.php
@@ -7,13 +7,14 @@ $dbname = "X33670851";
 $mysqli = new mysqli($host, $user, $password, $dbname);
 
 if ($mysqli->connect_error) {
-	echo "Connection failed.";
 	die("Connection failed: " . $mysqli->connect_error);
 }
 
 $userinput = $_GET['userinput'];
 
-$query = "SELECT * FROM Products WHERE ProductName LIKE '{$userinput}'";
+$query = "SELECT * FROM Products, Stock 
+		  WHERE Products.ProductID = Stock.ProductID
+		  AND ProductName LIKE '%{$userinput}%'";
 $result = $mysqli->query($query);
 
 $products = array();
@@ -26,5 +27,5 @@ $result->free_result();
 
 $mysqli->close();
 
-echo json_encode($result);
+echo json_encode($products);
 ?>
