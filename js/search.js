da748145297a3a8dc624cb15e6eed4fcfadc81b1
33670851 <33670851@ceto.murdoch.edu.au>
2021-10-28 13:07:44 +0800

    Added images to products

--- js/search.js
+++ js/search.js
@@ -10,9 +10,10 @@ function displayItems(products) {
     products.forEach((element) => {
         let productDiv = $('<div/>')
                             .addClass("grid-item")
-                            .html(`<h2>${element.ProductName}</h2>
-                                   <p>${element.ProductDesc}</p>
-                                   <p>$${element.Price}</p>`);
+                            .html(`<img class="product-img" src=images/cpu.jpg>
+                                   <h2>${element.ProductName}</h2>
+                                   <p class="price">$${element.Price}</p>
+                                   <p class="product-desc">${element.ProductDesc}</p>`);
 
         $('.grid-container').append(productDiv);
     });   
